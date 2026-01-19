b5a8ea37925475d9429cc0f1b259a1dd20edea47
Vasily Shashov <shashovva@mail.ru>
2026-01-19 15:16:02 +0300

    Идентификаторы

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Vasily on 19.01.2026.
+//
+
+import XCTest
+
+class MovieQuizUITests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+    }
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testScreenCast() throws {
+  
+    }
+    
+}
+    
